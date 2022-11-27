d083a0c702aae00996bd76a1754670fe89ab482b
Aimee <AimeeAngelia2022@Gmail.com>
2022-11-27 09:36:54 +0800

    调试hello world

--- item/src/vscode/App.java
+++ item/src/vscode/App.java
@@ -1,7 +1,7 @@
 package vscode;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
